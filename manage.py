e843dd5a963c4076fc489cc60a50a87732603e26
SHAMMAS <muhdshammas612@gmail.com>
2024-09-15 20:36:52 +0530

    tried to make it responsive using chatgpt

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avodhashop.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aecshop.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
